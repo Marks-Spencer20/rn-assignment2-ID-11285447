96b252b2b5eff14dd550d3c9bd45839fd1b7be45
Marks-Spencer20 <javisfrimpong.2003@gmail.com>
2024-05-25 01:11:14 +0000

    Editing the text component

--- MyNewProject/App.js
+++ MyNewProject/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        My name is <Text style={styles.boldText}>JAVIS FRIMPONG</Text>
+        </Text>
       <StatusBar style="auto" />
     </View>
   );
